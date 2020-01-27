2e54402788880cb355848d4b8a3e5e6685f4bc21
Peter <petr_adamovich@i.ua>
2020-01-27 22:37:44 +0200

    fixed navmenu

--- src/components/Navmenu/Navmenu.js
+++ src/components/Navmenu/Navmenu.js
@@ -5,12 +5,14 @@ import './NavMenu.css';
 const Navmenu = () => {
     return (
         <div className='navmenu'>
-            <div className='navmenu__items'>
-                <NavLink className='navmenu__item' activeClassName='active' exact to='/'>Home</NavLink>
-                <NavLink className='navmenu__item' activeClassName='active' to='/news'>News</NavLink>
-                <NavLink className='navmenu__item' activeClassName='active' to='/profile'>Profile</NavLink>
-                <NavLink className='navmenu__item' activeClassName='active' to='/login'>Login</NavLink>
-            </div>
+            {/* <div className='navmenu__items'> */}
+                <ul className='navmenu__items'>
+                    <li className='navmenu__item'> <NavLink className='link' activeClassName='active' exact to='/'>Home</NavLink></li>
+                    <li className='navmenu__item'><NavLink className='link' activeClassName='active' to='/news'>News</NavLink></li>
+                    <li className='navmenu__item'><NavLink className='link' activeClassName='active' to='/profile'>Profile</NavLink></li>
+                    <li className='navmenu__item'><NavLink className='link' activeClassName='active' to='/login'>Login</NavLink></li>
+                </ul>
+            {/* </div> */}
             
         </div>
     )
