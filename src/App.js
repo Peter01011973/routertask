8df1a0682ed4e037935d42558474f05a77de7a1e
Petro Maliar <maliar@zfort.com>
2020-01-31 19:21:20 +0200

    31 janury

--- src/App.js
+++ src/App.js
@@ -8,34 +8,19 @@ import Profile from './containers/Profile/Profile';
 import Login from './containers/Login/Login';
 import Login2 from './containers/Login2/Login2';
 import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
-import Logout from './containers/Logout/Logout';
-
-// TODO useEffect which get localStorage 'token' and setAuth 
+import Logout from './containers/Logout/Logout'; 
 
 function App({history}) {
   const [auth, setAuth] = useState(false);
-  // const [firstLaunch, setFirstLaunch] = useState(true);
   useEffect(
     ()=>console.log('I am an effect: ',auth),[auth]
   )
-
-  // const [count, setCount] = useState(0);
-  // useEffect(() => {
-  //   const id = setInterval(() => {
-  //     setCount(count + 1);
-  //     console.log(count);
-      
-  //   }, 1000);
-  //   return () => clearInterval(id);
-  // }, [count]);
-
 
   useEffect(
     ()=> {
       console.log('I am an effect for tracking localStorage!');
       const token = localStorage.getItem('token');
       if (token) setAuth(true);
-      // setFirstLaunch(false)
     }, []
   )
 
@@ -56,7 +41,7 @@ function App({history}) {
           <Route exact path='/'><Home /></Route>
           <Route path='/news'><News /></Route>
           <ProtectedRoute path='/profile' component={Profile} auth={auth}/>
-          {/* <Route path='/login2' component={Login2}/> */}
+          <Route path='/login2' component={Login2}/>
           // TODO component method to do in login
           <Route path='/logout' render = {props =><Logout handleLogout={handleLogout} {...props}/>}/>
           <Route path='/login' render = {props =><Login handleLogin={handleLogin} {...props}/>}/>
--- src/Services/FBAuthService/FBAuthService.js
+++ src/Services/FBAuthService/FBAuthService.js
@@ -1,10 +1,7 @@
-// import React from 'react';
 import axios from 'axios';
-// import AlertDialog from '../../components/UI/Dialog/Dialog';
 
 const tryAuth = async (authData) => {
-    const reqestData = { ...authData, returnSecureToken: true }
-    
+    const reqestData = { ...authData, returnSecureToken: true }    
     // let response = null;
     // try {
         return await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBqymITIAQwvv51M9pXu0jnJ2gA8ncXnTA', reqestData);
