ef377642e312d95d895e26314722a8fc1c681656
Peter <petr_adamovich@i.ua>
2020-02-01 13:38:28 +0200

    baseAPI

--- src/Services/FBAuthService/FBAuthService.js
+++ src/Services/FBAuthService/FBAuthService.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
+const baseAPI = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBqymITIAQwvv51M9pXu0jnJ2gA8ncXnTA';
 
 const tryAuth = async (authData) => {
     const reqestData = { ...authData, returnSecureToken: true }    
     // let response = null;
     // try {
-        return await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBqymITIAQwvv51M9pXu0jnJ2gA8ncXnTA', reqestData);
+        return await axios.post(baseAPI, reqestData);
     // } 
     // catch (e) {
     //     // alert('Everything is bad! Login failed!', e);
