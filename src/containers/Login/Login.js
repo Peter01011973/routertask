927ab9690f85b9b97d1ce24449b016cc54599cec
Peter <petr_adamovich@i.ua>
2020-02-01 17:33:55 +0200

    gg

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,13 +1,12 @@
-import React, {useState} from 'react';
+import React, { useEffect} from 'react';
 import './Login.css';
-import tryAuth from '../../Services/FBAuthService/FBAuthService';
 import CustomizedSnackbars from '../../components/UI/Snackbar/Snackbar';
 import AuthForm from '../../components/AuthForm/AuthForm';
 import useFetch from '../../hooks/useFetch';
 
 const Login = ({handleLogin, location, history}) => {
     // const [isLoading, setLoading] = useState(false);
-    const [isError, setError] = useState(false);
+    // const [isError, setError] = useState(false);
 
     const [{response, error, isLoading}, doFetch] = useFetch();
 
@@ -16,24 +15,16 @@ const Login = ({handleLogin, location, history}) => {
         
         const user = {
             email,
-            password: '111111'
+            password: '111111',
+            returnSecureToken: true
         }
 
-        setError(false);
-        setLoading(true);
+        // setError(false);
+        // setLoading(true);
         doFetch({
-            method: 'post',
+            // method: 'post',
             user
         });
-
-        useEffect(() => {
-            if (response && response.data && response.data.localId) {
-                localStorage.setItem('token', response.data.localId);
-                handleLogin();
-                setLoading(false);
-                if (location.state) { history.replace(location.state.from) }
-            }
-        }, [response])
         // try {
         //     const response = await tryAuth(authData)
 
@@ -43,10 +34,19 @@ const Login = ({handleLogin, location, history}) => {
         // }
     }
 
+    useEffect(() => {
+        if (response && response.data && response.data.localId) {
+            localStorage.setItem('token', response.data.localId);
+            handleLogin();
+            // setLoading(false);
+            // if (location.state) { history.replace(location.state.from) }
+        }
+    }, [response])
+
     return (
         <div className='login'>
             {isLoading && <h4>Loading ...</h4>}
-            {isError && <CustomizedSnackbars />}
+            {error && <CustomizedSnackbars />}
             <h1 className='login__title'>Authorization</h1>
             <AuthForm handleAuthSubmit = {handleAuthSubmit}/>
         </div>
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,8 +1,35 @@
-import React from 'react'
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+
 
 const useFetch = () => {
+    const baseAPI = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBqymITIAQwvv51M9pXu0jnJ2gA8ncXnTA';
+    const [isLoading, setIsLoading] = useState(false);
+    const [options, setOptions] = useState(null)
+    const [response, setResponse] = useState(null)
+    const [error, setError] = useState(null)
     
-    return 
+    const doFetch = options => {
+        setIsLoading(true)
+        setOptions(options)
+    }
+
+    useEffect(() => {
+        if (!isLoading) return
+        console.log('in useEffect', options);
+        
+        axios.post(baseAPI, options)
+            .then(response => {
+                setResponse(response)
+                setIsLoading(false)
+            })
+            .catch(error => {
+                setError(error)
+                setIsLoading(false)
+            })
+    },[isLoading])
+
+    return [{response, error, isLoading}, doFetch]
 }
 
 export default useFetch
